08693714cd955915eca05f8da9377427e21aef36
solonkonora <nkwadanora@gmail.com>
2024-03-31 08:14:38 +0100

    add files to the project

---
+++ src/commands/shorten.js
@@ -0,0 +1,20 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+const ShortUrl = sequelize.define('ShortUrl', {
+    full: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    short: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: Sequelize.fn('uuid_generate_v4') // Replace with PostgreSQL UUID generation function
+    },
+    clicks: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+    }
+});
+
+module.exports = ShortUrl;
