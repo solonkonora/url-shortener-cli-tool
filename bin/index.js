76adca815c13e92ee858dfcb0278b452888bbeb9
solonkonora <nkwadanora@gmail.com>
2024-04-03 13:27:46 +0100

    created db and connect it to the project

--- bin/index.js
+++ bin/index.js
@@ -1,40 +1,71 @@
 #! /usr/bin/env node
 //console.log("Hello World This is the first program!");
-// import { program } from 'commander';
 
 //creating objects with command line flag
 const { program } = require('commander');
+const { Pool } = require('pg');
+
+// Create a PostgreSQL pool
+const pool = new Pool({
+    user: 'nkwada',
+    host: 'localhost',
+    database: 'clitool',
+    password: 'norash',
+    port: 5432,
+});
 
 // 1) Simulating a list of shortened URLs
-const shortenedURLs = [
-    { id: 1, url: 'https://example.com/shorturl1' },
-    { id: 2, url: 'https://example.com/shorturl2' },
-    { id: 3, url: 'https://example.com/shorturl3' },
-];
-
 program
     .option('-l, --list', 'List all shortened URLs')
     .parse(process.argv);
 
 if (program.list) {
     console.log('Your shortened URLs:');
-    shortenedURLs.forEach((shortURL) => {
+    listShortenedURLs.forEach((shortURL) => {
         console.log(`ID: ${shortURL.id}, URL: ${shortURL.url}`);
     });
 }
 
-
-// 2) allow the user to be able to see how to use our cli tool ( for example : --help )
+// 2) Allow the user to be able to see how to use our CLI tool (for example: --help)
 
 program
-  .version('1.0.0')
-  .description('URL Shortener CLI Tool')
-  .command('shorten <url>')
-  .description('Shorten a long URL')
-  .action((url) => {
-    // Implement URL shortening logic here
-    console.log(`Shortened URL: [shortened-url]`);
-  });
+    .version('1.0.0')
+    .description('URL Shortener CLI Tool')
+    .command('shorten <url>')
+    .description('Shorten a long URL')
+    .action(async (url) => {
+        const shortenedURL = await generateShortURL(url);
+        console.log(`Shortened URL: ${shortenedURL}`);
+    });
+
+async function generateShortURL(url) {
+    const client = await pool.connect();
+    try {
+        // Insert the long URL into the database and retrieve the generated short URL
+        const query = 'INSERT INTO urls (long_url) VALUES ($1) RETURNING short_url';
+        const values = [url];
+        const result = await client.query(query, values);
+        return result.rows[0].short_url;
+    } finally {
+        client.release();
+    }
+}
+
+// retrieves short_urls from the database and display on the console
+async function listShortenedURLs() {
+    const client = await pool.connect();
+    try {
+      const query = 'SELECT id, short_url, long_url FROM urls';
+      const result = await client.query(query);
+  
+      console.log('Your shortened URLs:');
+      result.rows.forEach((row) => {
+        console.log(`ID: ${row.id}, Short URL: ${row.short_url}, Long URL: ${row.long_url}`);
+      });
+    } finally {
+      client.release();
+    }
+  }
 
 
 
