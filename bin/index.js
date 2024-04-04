8c837d1c01f34e7e1e3228ff77ec1028ceb5f298
solonkonora <nkwadanora@gmail.com>
2024-04-04 17:50:33 +0100

    connection to database established, installation of sequelize

--- bin/index.js
+++ bin/index.js
@@ -1,76 +1,24 @@
 #! /usr/bin/env node
-//console.log("Hello World This is the first program!");
-
-//creating objects with command line flag
 const { program } = require('commander');
-const { Pool } = require('pg');
-
-// Create a PostgreSQL pool
-const pool = new Pool({
-    user: 'nkwada',
-    host: 'localhost',
-    database: 'clitool',
-    password: 'norash',
-    port: 5432,
-});
-
-// 1) Simulating a list of shortened URLs
-program
-    .option('-l, --list', 'List all shortened URLs')
-    .parse(process.argv);
-
-if (program.list) {
-    console.log('Your shortened URLs:');
-    listShortenedURLs.forEach((shortURL) => {
-        console.log(`ID: ${shortURL.id}, URL: ${shortURL.url}`);
-    });
-}
-
-// 2) Allow the user to be able to see how to use our CLI tool (for example: --help)
+const shortenCommand = require('../commands/shorten.js');
+const sequelize = require('../db_sequelize.js'); 
+// const { program } = require('../commands/shorten.js');
 
 program
-    .version('1.0.0')
-    .description('URL Shortener CLI Tool')
-    .command('shorten <url>')
-    .description('Shorten a long URL')
-    .action(async (url) => {
-        const shortenedURL = await generateShortURL(url);
-        console.log(`Shortened URL: ${shortenedURL}`);
-    });
+  .name('url-shortener-cli-tool') 
+//   .version('1.0.0') 
+  .description('URL Shortener CLI Tool');
 
-async function generateShortURL(url) {
-    const client = await pool.connect();
-    try {
-        // Insert the long URL into the database and retrieve the generated short URL
-        const query = 'INSERT INTO urls (long_url) VALUES ($1) RETURNING short_url';
-        const values = [url];
-        const result = await client.query(query, values);
-        return result.rows[0].short_url;
-    } finally {
-        client.release();
-    }
-}
+program.addCommand(shortenCommand);
 
-// retrieves short_urls from the database and display on the console
-async function listShortenedURLs() {
-    const client = await pool.connect();
-    try {
-      const query = 'SELECT id, short_url, long_url FROM urls';
-      const result = await client.query(query);
-  
-      console.log('Your shortened URLs:');
-      result.rows.forEach((row) => {
-        console.log(`ID: ${row.id}, Short URL: ${row.short_url}, Long URL: ${row.long_url}`);
-      });
-    } finally {
-      client.release();
-    }
-  }
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Database connection has been established successfully.');
+    program.parse(process.argv);
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the database:', error);
+  });
 
-
-
-program.parse(process.argv);
-
-if (program.help) {
-    program.outputHelp();
-}
+  program.parse(process.argv);
--- commands/shorten.js
+++ commands/shorten.js
@@ -1,20 +1,85 @@
+const { program } = require('commander');
 const { Sequelize, DataTypes } = require('sequelize');
 
-const ShortUrl = Sequelize.define('ShortUrl', {
-    full: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    short: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        defaultValue: Sequelize.fn('uuid_generate_v4') // Replace with PostgreSQL UUID generation function
-    },
-    clicks: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        defaultValue: 0,
-    }
+const sequelize = new Sequelize('clitool', 'nkwada', 'norash', {
+  host: 'localhost',
+  dialect: 'postgres',
 });
 
-module.exports = ShortUrl;
+const Url = sequelize.define('Url', {
+  longUrl: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  shortUrl: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+    unique: true,
+  },
+});
+
+program
+  .version('1.0.0')
+  .description('URL Shortener CLI Tool');
+
+program
+  //.command('shorten <url>', { name: 'shorten' })
+  .command('shorten <url>')
+  .description('Shorten a URL')
+  .action(async (url) => {
+    const shortenedURL = await generateShortURL(url);
+    console.log(`Shortened URL: ${shortenedURL}`);
+  });
+
+program
+  .command('list')
+  .description('List all shortened URLs')
+  .action(async () => {
+    await listShortenedURLs();
+  });
+
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+function generateRandomString(length) {
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomIndex);
+  }
+  return result;
+}
+
+async function generateShortURL(url) {
+  try {
+    let shortUrl;
+    let isUnique = false;
+    while (!isUnique) {
+      shortUrl = generateRandomString(8); // Generate a random string of length 8
+      const existingUrl = await Url.findOne({ where: { shortUrl } });
+      if (!existingUrl) {
+        isUnique = true;
+      }
+    }
+    const shortenedURL = await Url.create({
+      longUrl: url,
+      shortUrl: shortUrl,
+    });
+    return shortenedURL.shortUrl;
+  } catch (error) {
+    console.error('Error creating shortened URL:', error);
+  }
+}
+
+async function listShortenedURLs() {
+  try {
+    const shortenedURLs = await Url.findAll();
+    console.log('Your shortened URLs:');
+    shortenedURLs.forEach((shortURL) => {
+      console.log(`ID: ${shortURL.id}, URL: ${shortURL.longUrl}`);
+    });
+  } catch (error) {
+    console.error('Error retrieving shortened URLs:', error);
+  }
+}
+
+program.parse(process.argv);
